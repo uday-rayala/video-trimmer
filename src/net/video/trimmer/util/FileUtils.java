614c0c429c18803a5acaabd4b5964766676091a8
rudayaku <rudayaku@thoughtworks.com>
2011-01-15 19:17:39 +0530

    Added android tests

--- src/net/video/trimmer/util/FileUtils.java
+++ src/net/video/trimmer/util/FileUtils.java
@@ -8,22 +8,24 @@ import java.util.List;
 public class FileUtils {
 	public static String getTargetFileName(String inputFileName) {
 		final File file = new File(inputFileName).getAbsoluteFile();
+		final String fileName = file.getName();
+		
 		String[] filenames = file.getParentFile().list(new FilenameFilter() {
 			@Override
 			public boolean accept(File dir, String filename) {
-				return filename != null && filename.startsWith("trimmed-") && filename.endsWith(file.getName());
+				return filename != null && filename.startsWith("trimmed-") && filename.endsWith(fileName);
 			}
 		});
 		
 		int count = 0;
 		String targetFileName;
 		List<String> fileList = Arrays.asList(filenames);
-		for (;;){
-			String name = "trimmed-" + String.format("%03d", count++)+ "-" + file.getName();
-			if (!fileList.contains(name)) {targetFileName = name; break;}
-		}
-		String dir = file.getParent();
-		return ("/".equals(dir)? dir : dir + File.separator) +  targetFileName;
+		
+		do {
+			targetFileName = "trimmed-" + String.format("%03d", count++)+ "-" + fileName; 
+		} while(fileList.contains(targetFileName));
+		
+		return new File(file.getParent(), targetFileName).getPath();
 	}
 
 }
